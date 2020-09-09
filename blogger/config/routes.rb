2ed69280256d62a7a89146357bc0508206bbe1b4
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-09-09 12:01:20 -0300

    Tagging feature completed

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,8 +1,10 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Blogger::Application.routes.draw do
+
   root to: 'articles#index'
   resources :articles do
     resources :comments
   end
-  
+  resources :tags
+
 end
+
