55a1990d47637a8b8a7da234da661d589750a13e
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-09-09 16:40:42 -0300

    rbenv problem, working on Adding to the Model I4

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -2,7 +2,9 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
-  
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
   def tag_list
     tags.join(", ")
   end
@@ -12,6 +14,5 @@ class Article < ApplicationRecord
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
     self.tags = new_or_found_tags
   end
-  
   
 end
