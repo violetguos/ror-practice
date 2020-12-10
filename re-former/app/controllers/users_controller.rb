d9d668a81355808f8bd0e6a987b3b1b5fa96b8fd
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-12-10 19:34:57 -0400

    Finish forms

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    # render
+    @user = User.find(params[:id])
+  end
+
+  def update
+    # actually create in DB
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
+
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
