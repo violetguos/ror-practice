35427f55cc4fb0628d55636385d67b4ab1268236
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-12-22 16:34:23 -0400

    Delete about us

--- railsfriends/friends/config/routes.rb
+++ railsfriends/friends/config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # get 'home/index'
   get 'home/about'
   #root 'home#index'
-  root 'friends#index'
+  root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
