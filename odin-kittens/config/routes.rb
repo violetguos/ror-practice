750aeb0035c139499446bb0cbe5fe9ba46283de6
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-01-31 22:17:07 -0400

    Add basic kitten scaffold

--- odin-kittens/config/routes.rb
+++ odin-kittens/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :kittens
+  root 'kittens#index' # shortcut for the above
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
