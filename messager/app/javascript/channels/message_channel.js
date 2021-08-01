f4cd5d74ef2609224af685af3304ae7795ae47c0
Y. Violet Guo <violetguos@users.noreply.github.com>
2021-07-31 22:38:48 -0400

    barebone chatroom exercise

---
+++ messager/app/javascript/channels/message_channel.js
@@ -0,0 +1,27 @@
+import consumer from "./consumer"
+
+const messageChannel = consumer.subscriptions.create("MessageChannel", {
+  connected() {
+  },
+
+  disconnected() {
+  },
+
+  received(data) {
+    const messageDisplay = document.querySelector('#message-display')
+    messageDisplay.insertAdjacentHTML('beforeend', this.template(data))
+  },  
+
+  template(data) {
+    return `<article class="message">
+              <div class="message-header">
+                <p>${data.user.email}</p>
+              </div>
+              <div class="message-body">
+                <p>${data.body}</p>
+              </div>
+            </article>`
+  }
+});
+
+
